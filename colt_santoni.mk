f98209b29f78b4880b26cd1ef383ef2b540ec95d
rico192 <49857555+rico192@users.noreply.github.com>
2020-04-10 19:46:25 +0700

    Bring colt

--- colt_santoni.mk
+++ colt_santoni.mk
@@ -20,12 +20,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/du/config/common_full_phone.mk)
+# Inherit some common Colt stuff.
+$(call inherit-product, vendor/colt/config/common.mk)
 
 # Inherit from santoni device
 $(call inherit-product, device/xiaomi/santoni/device.mk)
 
+TARGET_BOOT_ANIMATION_RES := 720x1280 : Please change as per your device resolution
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4X
 PRODUCT_DEVICE := santoni
